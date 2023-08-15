140b73d28b709585c86f0dde06c4e7ebcc59c9c8
wlsgus5932 <wlsgus1149@naver.com>
2023-08-15 23:46:42 +0900

    colunm 속성들

--- src/main/java/jpabasic/Member.java
+++ src/main/java/jpabasic/Member.java
@@ -1,13 +1,37 @@
 package jpabasic;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class Member {
     @Id
     private Long id;
-    private String name;
+
+    @Column(name = "name")
+    private String username;
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lab
+    private String description;
+
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Member() {
+    }
 
     public Long getId() {
         return id;
