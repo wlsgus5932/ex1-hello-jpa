38c3ad58f1d21b7732ac3426f292876470de3ef2
wlsgus5932 <wlsgus1149@naver.com>
2023-08-15 23:57:09 +0900

    colunm 속성들

--- src/main/java/jpabasic/Member.java
+++ src/main/java/jpabasic/Member.java
@@ -10,6 +10,7 @@ public class Member {
 
     @Column(name = "name")
     private String username;
+
     private Integer age;
 
     @Enumerated(EnumType.STRING)
@@ -21,31 +22,9 @@ public class Member {
     @Temporal(TemporalType.TIMESTAMP)
     private Date lastModifiedDate;
 
-    @Lab
+    @Lob
     private String description;
 
-
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
     public Member() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
