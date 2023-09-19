9e00c4e62c0d255511ec8910c3ec5ce788051896
wlsgus5932 <wlsgus1149@naver.com>
2023-09-19 20:24:19 +0900

    test

--- src/main/java/jpabasic/JpaMain.java
+++ src/main/java/jpabasic/JpaMain.java
@@ -7,9 +7,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -18,21 +16,19 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            Movie movie = new Movie();
-            movie.setDirector("aaa");
-            movie.setActor("bbb");
-            movie.setName("바람");
-            movie.setPrice(10000);
-            em.persist(movie);
-
             Member member = new Member();
             member.setUsername("userA");
-            em.persist(member);
-
+            Member member1 = new Member();
+            member1.setUsername("userB");
+            Team team = new Team();
             List<Member> resultList = em.createQuery("select m from Member m", Member.class).getResultList();
-            for (Member member1 : resultList) {
-                System.out.println(member1);
+            team.setMembers(resultList);
+
+            List<Team> selectTFromTeamT = em.createQuery("select t from Team t", Team.class).getResultList();
+            for (Team team1 : selectTFromTeamT) {
+                System.out.println(team1);
             }
+
 
             em.flush();
             em.clear();
